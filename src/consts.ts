f83997851a29bd49724d824d2cc4d6b63980b8db
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-06-09 09:35:00 +0800

    Set site title and description in head

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tunequil Journal';
+export const SITE_DESCRIPTION = '<meta name="description" content="Explore uplifting music recommendations and personal reflections on songs that inspire courage, optimism, and emotional balance." />\n!';
