4dcbe518c9f86e845fc80f9cf5b15c645a78900d
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-06-12 11:58:54 +0800

    setup sitemap and robots.txt.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://tunequil-journal.netlify.app/',
     integrations: [mdx(), sitemap()],
     vite: {
         plugins: [tailwindcss()],
