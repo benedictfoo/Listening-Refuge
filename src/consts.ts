79780b13238947ce214a172128d2a7cfc1f267b0
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-06-17 15:30:28 +0800

    style fixes

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@
 
 export const SITE_TITLE = 'Listening Refuge';
 export const SITE_DESCRIPTION = '<meta name="description" content="Explore uplifting and comforting song recommendations" />\n!';
-export const COLOR_PRIMARY = 'bg-cyan-100'
+export const COLOR_PRIMARY = 'bg-sky-300'
 export const COLOR_SECONDARY = 'bg-orange-50'
 export const COLOR_SECONDARY_SM = 'sm:bg-orange-50'
