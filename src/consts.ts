da7a9e496dc242f4d2a76d077f5777448ca79c40
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-06-17 14:59:02 +0800

    change descriptions and title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Tunequil Journal';
+export const SITE_TITLE = 'Listening Refuge';
 export const SITE_DESCRIPTION = '<meta name="description" content="Explore uplifting and comforting song recommendations" />\n!';
 export const COLOR_PRIMARY = 'bg-cyan-100'
 export const COLOR_SECONDARY = 'bg-orange-50'
