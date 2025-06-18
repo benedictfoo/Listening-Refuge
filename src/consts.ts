0b26d711d93efaf2b0c813aa2f04f097158198de
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-06-18 10:24:50 +0800

    fix site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Listening Refuge';
-export const SITE_DESCRIPTION = '<meta name="description" content="Explore uplifting and comforting song recommendations" />\n!';
+export const SITE_DESCRIPTION = "Explore uplifting and comforting song recommendations"
 export const COLOR_PRIMARY = 'bg-sky-300'
 export const COLOR_SECONDARY = 'bg-orange-50'
 export const COLOR_SECONDARY_SM = 'sm:bg-orange-50'
