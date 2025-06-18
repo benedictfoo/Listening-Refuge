027b2c0a25e173a05302231f229083dbe27223ba
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-06-18 14:56:04 +0800

    move favicons to folder. change consts color and description and add fallbackOG, style colors with classes only

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import fallbackOGIMage from '../public/favicon/android-chrome-512x512.png'
 
 export const SITE_TITLE = 'Listening Refuge';
-export const SITE_DESCRIPTION = "Explore uplifting and comforting song recommendations"
-export const COLOR_PRIMARY = 'bg-sky-300'
-export const COLOR_SECONDARY = 'bg-orange-50'
-export const COLOR_SECONDARY_SM = 'sm:bg-orange-50'
+export const SITE_DESCRIPTION = "Sharing uplifting And calming songs, one at a time."
+
+
+export const FALLBACK_OG_IMAGE = fallbackOGIMage
