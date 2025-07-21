db0bc76c2142c34a7f7b2bdd6c0330b08587c923
benfoo <200115050+benedictfoo@users.noreply.github.com>
2025-07-22 05:35:29 +0800

    remove favicons. fix layout issue on home page(image not in layout)

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-import fallbackOGIMage from '../public/favicon/android-chrome-512x512.png'
+
 
 export const SITE_TITLE = 'Listening Refuge';
 export const SITE_DESCRIPTION = "Sharing uplifting And calming songs, one at a time."
 export const POSTS_PER_PAGE = 3
 
-export const FALLBACK_OG_IMAGE = fallbackOGIMage
